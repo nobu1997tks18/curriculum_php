6afa3c4d8944a63706ea82515aafee5029f54a48
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2021-01-28 00:33:49 +0900

    finish 2-13

---
+++ 2-13/index.php
@@ -0,0 +1,17 @@
+<?php
+$x = 10.8;
+$word = "Hello";
+?>
+
+<p>ceil=<?php echo ceil($x)?><p>
+<p>floor=<?php echo floor($x)?><p>
+<p>round=<?php echo round($x)?><p>
+<p>10 * pi()=<?php echo 10*pi()?><p>
+<p>mt_rand=<?php echo mt_rand(1,100)?><p>
+<p>strlen=<?php echo strlen($word)?><p>
+<p>strpos=<?php echo strpos($word,"e")?><p>
+<p>substr=<?php echo substr($word,2,3)?><p>
+<p>str_replace=<?php echo str_replace("Hell","Tar",$word)?><p>
+<p>printf=<?php printf("%s駅までは徒歩%d分です","東京",10)?><p>
+<p>sprintf=<?php echo sprintf("%s駅までは徒歩%d分です","東京",10)?><p>
+
