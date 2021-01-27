6ab75c00ca8ca210c46b5d261609e3846ee07f6d
nobu1997tks18 <n.k.1997.tks.18@gmail.com>
2021-01-28 01:09:58 +0900

    finish 2-15

---
+++ 2-16/index.php
@@ -0,0 +1,28 @@
+<?php
+  $testFile="test.txt";
+  $content ="こんにちわ";
+
+  if(is_writable($testFile)){
+      $fp = fopen($testFile, "a");
+      fwrite($fp, $content);
+      fclose($fp);
+      echo "finish writing!";
+  }else{
+    echo "not writable!";
+    exit;
+  };
+?>
+
+<?php
+  $test_file = "test2.txt";
+  if(is_readable($test_file)){
+    $fp = fopen($test_file,"r");
+    while($line = fgets($fp)){
+      echo $line."<br>";
+    }
+    fclose($fp);
+  }else{
+    echo "not readable!!";
+    exit;
+  }
+?>
